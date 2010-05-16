2e6a87fcee49d033d3959fb850dd994ee7d79008
travisutk <travisutk@b5edc96b-3c45-4109-8ffe-cb651e37b440>
2010-05-16 05:40:56 +0000

    Info on badge flashing.
    git-svn-id: https://nhbadge.svn.sourceforge.net/svnroot/nhbadge@47 b5edc96b-3c45-4109-8ffe-cb651e37b440

--- dist/flashBadge.bat
+++ dist/flashBadge.bat
@@ -1,6 +1,8 @@
 
 set GOODFET=%1
 echo Using Port %GOODFET%
+
+msp430\goodfet.exe info
 msp430\goodfet.exe erase
 msp430\goodfet.exe flash openbeacontag.hex
 msp430\goodfet.exe verify openbeacontag.hex
