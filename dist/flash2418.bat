d5c15d6f2d813db82682f62c710bf25047c06987
travisutk <travisutk@b5edc96b-3c45-4109-8ffe-cb651e37b440>
2010-06-25 21:13:53 +0000

    New distribution, support for 2418 chips.
    git-svn-id: https://nhbadge.svn.sourceforge.net/svnroot/nhbadge@116 b5edc96b-3c45-4109-8ffe-cb651e37b440

--- dist/flash2418.bat
+++ dist/flash2418.bat
@@ -3,6 +3,6 @@
 goodfet\gf430.exe info 
 goodfet\gf430.exe erase
 goodfet\gf430.exe serial
-goodfet\gf430.exe flash openbeacontag.hex
-goodfet\gf430.exe verify openbeacontag.hex
+goodfet\gf430.exe flash 2418.hex
+goodfet\gf430.exe verify 2418.hex
 goodfet\gf430.exe run 
---
+++ dist/flash2618.bat
@@ -0,0 +1,8 @@
+
+
+goodfet\gf430.exe info 
+goodfet\gf430.exe erase
+goodfet\gf430.exe serial
+goodfet\gf430.exe flash 2618.hex
+goodfet\gf430.exe verify 2618.hex
+goodfet\gf430.exe run 
