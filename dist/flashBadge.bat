42fab3f3c79aba53cd88de5d74ed5d1024a6384b
travisutk <travisutk@b5edc96b-3c45-4109-8ffe-cb651e37b440>
2010-06-25 21:26:58 +0000

    git-svn-id: https://nhbadge.svn.sourceforge.net/svnroot/nhbadge@118 b5edc96b-3c45-4109-8ffe-cb651e37b440

---
+++ dist/flashBadge.bat
@@ -0,0 +1,4 @@
+REM flashBadge.bat has been replaced with two new scripts.
+REM Run flash2618.bat for boards with an 80-pin MSP430F2618.
+REM Run flash2418.bat for boards with a  64-pin MSP430F2418.
+
